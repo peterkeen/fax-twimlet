46856cc2d205d0cedc5d587e2b59f9ce6829da79
Pete Keen <pete@petekeen.net>
2017-03-31 14:18:32 -0400

    Add error and delivery handling

--- app.rb
+++ app.rb
@@ -12,11 +12,36 @@ Click this link to view the fax:
 
 #{params[:MediaUrl]}
 """
+    subject = "Fax received from #{params[:From]}"
+    send_mail(params[:email], subject, body)
+  elsif params[:FaxStatus] == 'delivered'
+    body = """Your fax has been delivered to #{params[:To]}.
 
+Click this link to view the fax:
+
+#{params[:MediaUrl]}
+"""
+    subject = "Fax delivered to #{params[:to]}"
+    send_mail(params[:email], subject, body)
+  elsif params[:FaxStatus] == 'failed'
+    body = """Your fax to #{params[:To]} has failed.
+
+Error #{params[:ErrorCode]}: #{params[:ErrorMessage]}
+"""
+    subject = "Fax to #{params[:to]} failed"
+    send_mail(params[:email], subject, body)
+  end
+
+  Twilio::TwiML::Response.new do |r|
+    r.Receive action: request.url
+  end.text
+end
+
+def send_mail(to, subject, body)
     Pony.mail(
       to: params[:email],
-      from: 'fax@bugsplat.info',
-      subject: "Fax received from #{params[:From]}",
+      from: ENV['SMTP_SERVER_FROM'],
+      subject: subject,
       body: body,
       via: :smtp,
       via_options: {
@@ -25,13 +50,8 @@ Click this link to view the fax:
         user_name:      ENV['SMTP_SERVER_USERNAME'],
         password:       ENV['SMTP_SERVER_PASSWORD'],
         authentication: 'plain',
-        domain:         'feeds.bugsplat.info',
+        domain:         ENV['SMTP_SERVER_DOMAIN'],
         enable_starttls_auto: true
       }
     )
-  end
-
-  Twilio::TwiML::Response.new do |r|
-    r.Receive action: request.url
-  end.text
 end
